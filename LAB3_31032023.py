2bb828d7b8ccc1a05c3ed01e12093d4c0dbd041d
suryasera <126846745+suryasera@users.noreply.github.com>
2023-03-31 11:49:16 +0530

    Update LAB3_31032023.py

--- LAB3_31032023.py
+++ LAB3_31032023.py
@@ -4,17 +4,18 @@
 import RPi.GPIO as GPIO
 #Import time function Library
 import time
-a = 0
-while a <= 5:
+
 #Configure GPIO in Raspberry Pi BCM Mode
   GPIO.setmode(GPIO.BCM) 
 #Configure GPIO Pin 17 as output
   GPIO.setup(17,GPIO.OUT) 
+  a = 0
+  while a <= 5:
 #Set GPIO Pin to High
-  GPIO.output(17,GPIO.HIGH)
+    GPIO.output(17,GPIO.HIGH)
 #Wait for 1sec
-  time.sleep(1)
+    time.sleep(1)
 #Set GPIO Pin to Low
-  GPIO.output(17,GPIO.LOW)
-  time.sleep(1)
-  a+=1
+    GPIO.output(17,GPIO.LOW)
+    time.sleep(1)
+    a+=1
