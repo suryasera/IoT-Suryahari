49d75194ee69a3f65e9309848c0b0f3e4657b8b0
suryasera <126846745+suryasera@users.noreply.github.com>
2023-03-31 11:45:07 +0530

    Update LAB3_31032023.py

--- LAB3_31032023.py
+++ LAB3_31032023.py
@@ -4,8 +4,8 @@
 import RPi.GPIO as GPIO
 #Import time function Library
 import time
-a = 0
-while a <= 5
+a=0
+while a<=5
 #Configure GPIO in Raspberry Pi BCM Mode
   GPIO.setmode(GPIO.BCM) 
 #Configure GPIO Pin 17 as output
