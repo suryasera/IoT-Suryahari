0295d5fbe463f872af5c0b5bd8bbf32827a35322
suryasera <126846745+suryasera@users.noreply.github.com>
2023-05-24 16:34:47 +0530

    Update publisher.py

--- mqtt_protocol/publisher.py
+++ mqtt_protocol/publisher.py
@@ -4,6 +4,6 @@ import paho.mqtt.client as mqtt
 payload="Hello:
 topic="IOT/test"
 client = mqtt.Client()
-client.connect('0.0.0.0',1883,60)
+client.connect('34.100.199.201',1883,60)
 (rc,mid)=client.publish(topic,payload);
 client.disconnect();
