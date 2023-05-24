a9f2e21a2dbd50126c31122dbc2ba771dc8361f2
suryasera <126846745+suryasera@users.noreply.github.com>
2023-05-24 17:39:11 +0530

    Update sub.py

--- mqtt_protocol/sub.py
+++ mqtt_protocol/sub.py
@@ -1,36 +1,44 @@
 import paho.mqtt.client as paho
-#pip3 install paho-mqtt
+import pymysql
 global mqttclient;
 global broker;
 global port;
-
-
-broker = "34.100.199.201";
+broker = 34.100.199.201;
 port = 1883;
-
 client_uniq = "pubclient_123"
-
 mqttclient = paho.Client(client_uniq, True) 
-
+# def storeData(mesg,t):
+#  #Create a connection to MySQL Database 
+#  conn =pymysql.connect(database="Iotdatap",user="user2",password="Pass1234",host="localhost")
+#  #Create a MySQL Cursor to that executes the SQLs
+#  cur=conn.cursor()
+#  #Create a dictonary containing the fields, name, age and place
+#  data={'topic':t,'data':mesg}
+#  #Execute the SQL to write data to the database
+#  cur.execute("INSERT INTO Sensordata(Topic , Sensorvalue)VALUES(%(topic)s,%(data)s);",data)
+#  print("Data added")
+#  #Close the cursor
+#  cur.close()
+#  #Commit the data to the database
+#  conn.commit()
+#  #Close the connection to the database
+#  conn.close()
 def test(client, userdata, message):
-  print("client:"+ str(client))
-  print("userdata:"+ userdata)
-  print("message:"+ message.payload.decode())
-
+ print("client:"+ str(client))
+ print("userdata:"+ str(userdata))
+ print("message:"+ str(message.payload.decode()))
+#  storeData(message.payload.decode(),message.topic)
 def _on_message(client, userdata, msg):
-# 	print("Received: Topic: %s Body: %s", msg.topic, msg.payload)
-	print(msg.topic+" "+str(msg.payload))
-	 
+# print("Received: Topic: %s Body: %s", msg.topic, msg.payload)
+ print(msg.topic+" "+str(msg.payload))
 #Subscribed Topics 
 def _on_connect(mqttclient, userdata, flags, rc):
-# 	print("New Client: "+str(mqttclient)+ " connected")
-# 	print(rc)
-	mqttclient.subscribe("IOT/#", qos=0)	
-  
+# print("New Client: "+str(mqttclient)+ " connected")
+# print(rc)
+ mqttclient.subscribe("IOT/#", qos=0)
+ 
 mqttclient.message_callback_add("IOT/test", test)
-
 mqttclient.connect(broker, port, keepalive=1, bind_address="")
-  
+ 
 mqttclient.on_connect = _on_connect
-
 mqttclient.loop_forever()
